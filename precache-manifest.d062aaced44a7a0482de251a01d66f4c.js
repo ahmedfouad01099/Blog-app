1c7306fa0752999ed339f9a7a593ac694b232e0f
Ahmed Fouad <ahmedfouad01099@gmail.com>
2021-01-13 00:17:49 +0200

    Updates

---
+++ precache-manifest.d062aaced44a7a0482de251a01d66f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6978b2f3016258c9e05d",
+    "url": "/static/css/main.76e0db28.chunk.css"
+  },
+  {
+    "revision": "6978b2f3016258c9e05d",
+    "url": "/static/js/main.6978b2f3.chunk.js"
+  },
+  {
+    "revision": "d9fd9a03b80d046a6111",
+    "url": "/static/js/1.d9fd9a03.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "471cd97f66b55ea02e1277d57118817a",
+    "url": "/index.html"
+  }
+];
